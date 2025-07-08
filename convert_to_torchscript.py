61c88cd3f96527450d36ec540d843b878d4bfe60
rexxa <rexxa.som@kakaoenterprise.com>
2025-07-08 18:58:24 +0900

    Update convert_to_torchscript.py

--- convert_to_torchscript.py
+++ convert_to_torchscript.py
@@ -50,7 +50,7 @@ config = {
 
 # 모델 로드 및 TorchScript 변환
 checkpoint_path = "/hanmail/users/rexxa.som/jupyter/my_checkpoints3/lask.ckpt"
-output_path = "/Users/rexxa.som/github/triton_inference/triton_models/deep_hashing/1/model.pt"
+output_path = "/hanmail/users/rexxa.som/github/triton_inference/triton_models/deep_hashing/1/model.pt"
 
 # 1. 체크포인트를 로드하기 위한 원래 DeepHashingModel (LightningModule) 정의
 #    load_from_checkpoint가 이 클래스 정의를 필요로 합니다.
